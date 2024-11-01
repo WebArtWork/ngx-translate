392039e8cdf78061d9ff112cda3cbcf447ff5032
Honchar Denys <crackeraki@gmail.com>
2024-11-01 12:25:00 +0200

    Update code based on new forms with templates

--- translate.directive.ts
+++ translate.directive.ts
@@ -1,11 +1,16 @@
 import { OnInit, Directive, ElementRef } from '@angular/core';
-import { TranslateService } from 'src/app/modules/translate/translate.service';
+import { TranslateService } from 'src/app/core/modules/translate/translate.service';
 
 @Directive({
 	selector: '[translate]'
 })
 export class TranslateDirective implements OnInit {
-	constructor(public elementRef: ElementRef, private tr: TranslateService) {}
+	constructor(public elementRef: ElementRef, private tr: TranslateService) { }
+
+	/**
+	 * On initialization, this directive replaces the innerHTML of the element
+	 * with the translated version of the text.
+	 */
 	ngOnInit() {
 		this.elementRef.nativeElement.innerHTML = this.tr.translate(
 			this.elementRef.nativeElement.innerHTML,
